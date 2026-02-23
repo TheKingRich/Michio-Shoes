a7f46e675bef1bd1b46a6b1596300f2c39f60b36
Eugene <85484474+TheKingRich@users.noreply.github.com>
2026-02-23 20:02:23 +0700

    readmore.js

--- readmore.js
+++ readmore.js
@@ -1,31 +1,43 @@
-const cari = document.querySelectorAll('.sepatua p');
-for ( let i = 0; i < cari.length; i++ ){
-  const pisahkan = cari[i].innerHTML;
-  const jumlahkarakter = 300;
-  if ( pisahkan.length > jumlahkarakter) {
-  const treadless = pisahkan.substr(0, jumlahkarakter);
-  const treadless1 = treadless.substr(0, treadless.lastIndexOf(" ") + 1);
-  const readmore = pisahkan.substr(treadless1.length, pisahkan.length);
-  cari[i].innerHTML = treadless1;
+// Mengambil semua paragraf di dalam info produk
+const cari = document.querySelectorAll('.product-info p');
 
-  const createtombol = document.createElement('button');
-  const kata = document.createTextNode('Readmore');
-  createtombol.appendChild(kata);
-  createtombol.setAttribute('class', 'read-more');
-  cari[i].append(createtombol);
+for (let i = 0; i < cari.length; i++) {
+    const pisahkan = cari[i].innerHTML;
+    const jumlahkarakter = 200; // Dikurangi sedikit agar lebih rapi di kartu produk
 
+    if (pisahkan.length > jumlahkarakter) {
+        const treadless = pisahkan.substr(0, jumlahkarakter);
+        const treadless1 = treadless.substr(0, treadless.lastIndexOf(" ") + 1);
+        const readmore = pisahkan.substr(treadless1.length, pisahkan.length);
+        
+        // Set tampilan awal (teks terpotong)
+        cari[i].innerHTML = treadless1;
 
-  createtombol.onclick = function(){
-    if ( this.className.indexOf('read-more') > -1) {
-          cari[i].innerHTML = treadless1 + readmore;
-           this.innerText = "Readless";
-           this.classList.remove('read-more');
-          }else {
-           cari[i].innerHTML = treadless;
-           this.innerText = "Readmore";
-           this.classList.add('read-more');
-         }
-         cari[i].append(this);
-         };
-}
+        // Membuat tombol dengan class yang sudah kita buat di CSS
+        const createtombol = document.createElement('button');
+        createtombol.innerText = 'Read More';
+        createtombol.setAttribute('class', 'read-more-btn'); 
+        
+        // Styling tambahan via JS agar pas di posisi teks
+        createtombol.style.marginLeft = "5px";
+        createtombol.style.display = "inline-block";
+        
+        cari[i].append(createtombol);
+
+        createtombol.onclick = function() {
+            // Jika saat ini sedang menampilkan teks pendek
+            if (this.innerText === "Read More") {
+                cari[i].innerHTML = treadless1 + readmore;
+                this.innerText = "Read Less";
+                this.classList.remove('read-more'); // Menghindari konflik logic lama
+            } else {
+                // Jika sedang menampilkan teks lengkap
+                cari[i].innerHTML = treadless1;
+                this.innerText = "Read More";
+                this.classList.add('read-more');
+            }
+            // Tempelkan kembali tombolnya ke dalam paragraf
+            cari[i].append(this);
+        };
+    }
 }
